2259ab5a7b3ca5f88fa33b346135a35ef18352f4
cameronphan <music.cameronphan@gmail.com>
2026-05-09 10:22:41 -0400

    Update HomeClient.tsx

--- src/app/HomeClient.tsx
+++ src/app/HomeClient.tsx
@@ -104,12 +104,12 @@ export default function HomeClient() {
           <div dangerouslySetInnerHTML={{ __html: `
             <div class="tt-widget">
               <div class="tt-widget-fallback">
-                <p><a href="https://www.tickettailor.com/all-tickets/cameronphanmusic/?ref=website_widget" target="_blank">Click here to buy tickets</a></p>
+                <p><a href="https://www.tickettailor.com/all-tickets-by-event/cameronphanmusic/?ref=website_widget&show_search_filter=true&show_date_filter=true&show_sort=true" target="_blank">Click here to buy tickets</a><br /><small><a href="https://www.tickettailor.com?rf=wdg_305090" class="tt-widget-powered">Sell tickets online with Ticket Tailor</a></small></p>
               </div>
               <script src="https://cdn.tickettailor.com/js/widgets/min/widget.js"
-                data-url="https://www.tickettailor.com/all-tickets/cameronphanmusic/?ref=website_widget&show_search_filter=true&show_date_filter=true&show_sort=true"
-                data-type="inline" data-inline-minimal="true" data-inline-show-logo="false"
-                data-inline-bg-fill="false" data-inline-inherit-ref-from-url-param=""
+                data-url="https://www.tickettailor.com/all-tickets-by-event/cameronphanmusic/?ref=website_widget&show_search_filter=true&show_date_filter=true&show_sort=true"
+                data-type="inline" data-inline-minimal="false" data-inline-show-logo="false"
+                data-inline-bg-fill="true" data-inline-inherit-ref-from-url-param=""
                 data-inline-ref="website_widget"></script>
             </div>
           `}} />
