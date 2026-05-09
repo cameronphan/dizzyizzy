c0b2c2dbc6dba5980308c09b8db15ab67eb14566
cameronphan <music.cameronphan@gmail.com>
2026-05-09 10:26:47 -0400

    Update HomeClient.tsx

--- src/app/HomeClient.tsx
+++ src/app/HomeClient.tsx
@@ -109,7 +109,7 @@ export default function HomeClient() {
               <script src="https://cdn.tickettailor.com/js/widgets/min/widget.js"
                 data-url="https://www.tickettailor.com/all-tickets-by-event/cameronphanmusic/?ref=website_widget&show_search_filter=true&show_date_filter=true&show_sort=true"
                 data-type="inline" data-inline-minimal="false" data-inline-show-logo="false"
-                data-inline-bg-fill="true" data-inline-inherit-ref-from-url-param=""
+                data-inline-bg-fill="false" data-inline-inherit-ref-from-url-param=""
                 data-inline-ref="website_widget"></script>
             </div>
           `}} />
